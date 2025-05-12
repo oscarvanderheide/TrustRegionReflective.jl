cf859a5e1162796565570aba2c3119b6becb2359
oscar <oscarvanderheide@gmail.com>
2025-05-12 14:49:33 +0200

    Update docstring of TRFOptions

--- src/TrustRegionReflective.jl
+++ src/TrustRegionReflective.jl
@@ -6,29 +6,27 @@ using TimerOutputs
 using CUDA # Added for CUDA.jl direct usage
 
 """
-    struct TRFOptions{T<:Real, I<:Int, B<:Bool}
+    struct TRFOptions{T<:Real}
 
 Configuration options for the Trust Region Reflective (TRF) solver.
 
 # Type Parameters
 - `T<:Real`: The floating-point type for tolerances and ratios.
-- `I<:Int`: The integer type for iteration counts.
-- `B<:Bool`: The boolean type for flags.
 
 # Fields
 - `min_ratio::T`: The minimum acceptable ratio of actual to predicted reduction. A step is a
 ccepted if the ratio is above this value. Typically between 0 and 0.25.
-- `max_iter_trf::I`: The maximum number of iterations for the main Trust Region Reflective a
+- `max_iter_trf::Int`: The maximum number of iterations for the main Trust Region Reflective a
 lgorithm.
-- `max_iter_steihaug::I`: The maximum number of iterations for the Steihaug-Toint conjugate 
+- `max_iter_steihaug::Int`: The maximum number of iterations for the Steihaug-Toint conjugate 
 gradient subproblem solver.
 - `tol_steihaug::T`: The tolerance for the Steihaug-Toint subproblem. Iterations stop when t
 he norm of the residual is below this tolerance.
 - `init_scale_radius::T`: The initial scaling factor for the trust region radius, typically 
 applied to the norm of the initial `x0`.
-- `save_every_iter::B`: A boolean flag indicating whether to save intermediate results (e.g.
+- `save_every_iter::Bool`: A boolean flag indicating whether to save intermediate results (e.g.
 , `x`, `f`, `r`) at each iteration. This can be memory-intensive.
-- `modfified_reduction_for_ratio::B`: A boolean flag indicating whether to use a modified
+- `modfified_reduction_for_ratio::Bool`: A boolean flag indicating whether to use a modified
 reduction for the ratio calculation that takes into account the coleman li scaling factors.
 """
 @kwdef struct TRFOptions{T<:Real}
